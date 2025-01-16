ddd308925f0ca91aef7f2a40225a05dd078f8a80
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-01-16 15:50:06 -0500

    completed sidebar with icons

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -2,18 +2,87 @@
 import React from "react";
 import UserItem from "@/components/ui/UserItem";
 import {
+  BellIcon,
+  Cookie,
+  CreditCard,
+  Inbox,
+  MessageSquare,
+  Settings,
+  User,
+} from "lucide-react";
+import {
   Command,
-  CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
   CommandList,
   CommandSeparator,
-  CommandShortcut,
 } from "@/components/ui/command";
 
 const Sidebar = () => {
+  // type for menu items
+  type MenuItem = {
+    link: string;
+    icon: React.ReactNode;
+    text: string;
+  };
+
+  // type for menu groups
+  type MenuGroup = {
+    group: string;
+    items: MenuItem[];
+  };
+
+  // define the menu items for the sidebar
+  const menuList: MenuGroup[] = [
+    {
+      group: "General",
+      items: [
+        {
+          link: "/",
+          icon: <User />,
+          text: "Profile",
+        },
+        {
+          link: "/",
+          icon: <Inbox />,
+          text: "Inbox",
+        },
+        {
+          link: "/",
+          icon: <CreditCard />,
+          text: "Billing",
+        },
+        {
+          link: "/",
+          icon: <BellIcon />,
+          text: "Notifications",
+        },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        {
+          link: "/",
+          icon: <Settings />,
+          text: "General Settings",
+        },
+        {
+          link: "/",
+          icon: <Cookie />,
+          text: "Privacy",
+        },
+        {
+          link: "/",
+          icon: <MessageSquare />,
+          text: "Logs",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="flex min-h-screen w-[300px] min-w-[300px] flex-col gap-2 border p-4">
       <div>
@@ -24,23 +93,23 @@ const Sidebar = () => {
           <CommandInput placeholder="Type a command or search..." />
           <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Suggestions">
-              <CommandItem>Calendar</CommandItem>
-              <CommandItem>Search Emoji</CommandItem>
-              <CommandItem>Calculator</CommandItem>
-            </CommandGroup>
-            <CommandSeparator />
-            <CommandGroup heading="Settings">
-              <CommandItem>Profile</CommandItem>
-              <CommandItem>Billing</CommandItem>
-              <CommandItem>Settings</CommandItem>
-            </CommandGroup>
+            {menuList.map((menu: MenuGroup, menuIndex: number) => (
+              <React.Fragment key={menuIndex}>
+                <CommandGroup heading={menu.group}>
+                  {menu.items.map((item, itemIndex) => (
+                    <CommandItem key={itemIndex}>
+                      {item.icon}
+                      {item.text}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+                {menuIndex < menuList.length - 1 && <CommandSeparator />}
+              </React.Fragment>
+            ))}
           </CommandList>
         </Command>
       </div>
-      <div>Settings</div>
     </div>
   );
 };
-
 export default Sidebar;
