60fcb3d10f8373bd6f8acc0af3a28b8465612dde
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-01-15 22:47:52 -0500

    eslint and prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold flex justify-center">Home</h1>
+      <Button variant="destructive">Click me</Button>
     </div>
   );
 }
